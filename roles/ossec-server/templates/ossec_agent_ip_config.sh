3677decba6e19f2190c6ac701419a15b9f60cf9c
Fayssal Smawi <fayssal.smawi@gmail.com>
2018-06-05 17:26:02 +0000

    added config agent IP

--- roles/ossec-server/templates/ossec_agent_ip_config.sh
+++ roles/ossec-server/templates/ossec_agent_ip_config.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 # get hostname && IP
-AGENT_ID=$1
-HOSTNAME=$2
-AGENT_IP=$3
+HOSTNAME=$1
+AGENT_IP=$2
 
 # change the line with the hostname
-sed -i "/${AGENT_ID}\s${HOSTNAME}\s\(any\).*/ s/any/${AGENT_IP}/g" client.keys
+sed -i "/.*\s${HOSTNAME}\s\(any\).*/ s/any/${AGENT_IP}/g" client.keys
