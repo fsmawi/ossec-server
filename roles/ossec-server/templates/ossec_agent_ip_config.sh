8769f982e8c6d24456be17895618ae1d2efd5d4a
Fayssal Smawi <fayssal.smawi@gmail.com>
2018-06-05 17:46:06 +0000

    fixed path in config IP script

--- roles/ossec-server/templates/ossec_agent_ip_config.sh
+++ roles/ossec-server/templates/ossec_agent_ip_config.sh
@@ -5,4 +5,4 @@ HOSTNAME=$1
 AGENT_IP=$2
 
 # change the line with the hostname
-sed -i "/.*\s${HOSTNAME}\s\(any\).*/ s/any/${AGENT_IP}/g" client.keys
+sed -i "/.*\s${HOSTNAME}\s\(any\).*/ s/any/${AGENT_IP}/g" /var/ossec/etc/client.keys
